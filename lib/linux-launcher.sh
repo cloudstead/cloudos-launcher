733e700545edd44f23290f9fc0bba3f930ade4c8
jonathan <jonathan@kyuss.org>
2015-08-04 02:29:17 -0700

    add java opts

--- lib/linux-launcher.sh
+++ lib/linux-launcher.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# This script starts the Cloudstead Launcher server API.
+# When the API is up and running, if a windowing system is available it will opens a web browser
+# otherwise it prints the API endpoint to stdout
+#
 
 function die {
   echo "${1}" >&2 && exit 1
@@ -16,4 +21,8 @@ if [ -z "${JAR}" ] ; then
   die "No cloudos-launcher-*.jar found in ${THIS_DIR}/lib"
 fi
 
-${JRE_DIR}/bin/java -jar ${JAR}
+if [ -z "${JAVA_OPTS}" ] ; then
+  JAVA_OPTS="-Xms256m -Xmx1536m"
+fi
+
+${JRE_DIR}/bin/java -jar ${JAR} ${JAVA_OPTS}
