1e85a33a42569306d3459dd2077f69c1c1aff60a
Bojan Mojsilovic <bmojsilovic@itekako.com>
2015-09-21 18:23:54 +0200

    Redirect to 'add_cloudsteads' route if not cloudsteads are available.

--- csapp/app/js/routes/cloudsteads_route.js
+++ csapp/app/js/routes/cloudsteads_route.js
@@ -2,4 +2,10 @@ App.CloudsteadsRoute = App.ProtectedRoute.extend({
 	model: function() {
 		return App.CloudsteadModel.getAll();
 	},
+
+	afterModel: function(model) {
+		if (Ember.isEmpty(model)) {
+			this.transitionTo("add_cloudstead");
+		}
+	}
 });
